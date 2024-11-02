92aea9acacaf2e2dae0e81040a9b6cb08fa29297
Matheus Rodrigues <162641773+RodriguesMat@users.noreply.github.com>
2024-11-02 15:32:02 -0300

    Update rrt.py

--- src/map/rrt.py
+++ src/map/rrt.py
@@ -1,140 +1,111 @@
-
-import math
-from random import randrange
-from matplotlib import pyplot as plt
+import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+import math
+from math import *
+from matplotlib.colors import Normalize
+import random
 
-#Pontos de inicio e fim
-x_inicio = 40
-y_inicio = 40
-start = (x_inicio, y_inicio)
+pgmf = open('my_map.pgm', 'rb')
+image = plt.imread(pgmf)
 
-x_final = 250
-y_final = 250
-goal = (x_final, y_final)
+image_copia = 1.0 * (image > 250)
 
-#1 metro = 20 pixel
-grid_maximo = 401
-colunas = 401
-linhas = 401
+goal = (80, 325) 
+robo = (300, 25) 
 
-caminho_aberto = 1
-obstaculo = 0
+image_copia[goal[0]][goal[1]] = 0 
+image_copia[robo[0]][robo[1]] = 0   
 
-# Montando matrix do fafa
-pgmf = open('map.pgm', 'rb')
-matrix = plt.imread(pgmf)
+fig = plt.figure()
+fig.canvas.manager.set_window_title('Figura 1')
 
-matrix = (1.0 * (matrix > 220))
-
-class NodoArvore:
-    def _init_ (self,chave = None, pai = None, x=None,y=None):
-        self.id = chave
-        self.pai = pai
-        self.filhos = []
-        self.x = x
-        self.y = y
-       
-    def chegou(self,x,y,raio):
-        dist = math.sqrt((self.x - x)*2 + (self.y - y)*2)
-        if dist < raio:
-            return True
-        else:
-            return False
-   
-id_atual = 0
-raiz = NodoArvore(id_atual,None,x_inicio,y_inicio)
-id_atual += 1
-final = NodoArvore(id_atual,None,x_final,y_final)
-id_atual += 1
-
-
-# Nodos disponiveis para comparação
-nodes = [
-    NodoArvore(None, None, x_inicio, y_inicio),
-    NodoArvore(None, None, x_final, y_final)
-]
-
-# --------------------------------------------------------------------------------------------------------------------------------------
-def CoordenadaPoda(x_aleatorio,y_aleatorio,proximo_x,proximo_y):
-    dist_x_maior = abs(proximo_x - x_aleatorio)
-    dist_y_maior = abs(proximo_y - y_aleatorio)
-
-    if dist_x_maior == 0:
-        dist_x_maior = 1  # Evitar divisão por zero
-
-    if dist_y_maior == 0:
-        dist_y_maior = 1  # Evitar divisão por zero
-
-    hipotenusa_maior = math.sqrt(dist_x_maior*2 + dist_y_maior*2)
-
-    x_triangulo_menor = (20 * dist_x_maior) // hipotenusa_maior
-    y_triangulo_menor = (20 * dist_y_maior) // hipotenusa_maior
-
-    coordenada_x = abs(x_triangulo_menor - x_aleatorio)
-    coordenada_y = abs(y_triangulo_menor - y_aleatorio)
-
-    return coordenada_x, coordenada_y
-
-
-def NodoProximo(nodes,nodo_aleatorio, matrix):
-    menor_distancia = float('inf')
-    nodo_proximo = None
-    proximo_x, proximo_y = None, None
-
-    for node in nodes:
-        distancia = math.sqrt((node.x - nodo_aleatorio.x)*2 + (node.y - nodo_aleatorio.y)*2)
-        if distancia < menor_distancia:
-            menor_distancia = distancia
-            nodo_proximo = node
-   
-    proximo_x = nodo_proximo.x
-    proximo_y = nodo_proximo.y
-           
-    return nodo_proximo, proximo_x, proximo_y
-# --------------------------------------------------------------------------------------------------------------------------------------
-while True:
-    x_aleatorio = randrange(grid_maximo)
-    y_aleatorio = randrange(grid_maximo)
-    nodo_aleatorio = NodoArvore(None,None,x_aleatorio,y_aleatorio) #  
-    nodo_proximo,proximo_x,proximo_y = NodoProximo(nodes,nodo_aleatorio,matrix) #
-    x_poda,y_poda = CoordenadaPoda(x_aleatorio,y_aleatorio,proximo_x,proximo_y) #
-    nodo_novo = NodoArvore(id_atual,nodo_proximo.id,x_poda,y_poda)###########
-    nodes.append(nodo_novo)
-    nodo_proximo.filhos.append(nodo_novo)
-    id_atual += 1
-    if(nodo_novo.chegou(x_final, y_final, 20)):
-        break
-   
-path = []
-nodo = nodo_novo
-
-while True:
-    nodo = nodo.pai
-    print(nodo)
-    if nodo is None:
-        break
-    path.append(nodo)
-
-   
-# Visualize the path
-plt.imshow(matrix, interpolation='nearest', cmap='gray')
-
-# Plot the start and end points
-plt.scatter(x_inicio, y_inicio, color='green', marker='o', label='Start')
-plt.scatter(x_final, y_final, color='red', marker='o', label='Goal')
-
-# Plot the path
-x_path = [n.x for n in path]
-y_path = [n.y for n in path]
-plt.plot(x_path, y_path, color='blue', linestyle='-', linewidth=2, label='Path')
-
-plt.legend()
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('RRT Path')
+plt.imshow(image_copia, interpolation='nearest', cmap='gray')
+plt.title('Imagem inicial')
 plt.show()
 
+#algoritmo
+f_crescimento = 10
+arvore = [robo] 
+caminho = []
+pais =list()
+filhos = list()
+max_interacoes = 1000
 
---
-Matheus Rodrigues
+def achando_no_proximo(arvore, ponto_aleatorio):
+    menor_dist = math.dist((350,0), (0,350))
+    no_mais_prox = 0
+    for n in arvore:
+        dist = math.dist(n, ponto_aleatorio)
+        if dist < menor_dist:
+            menor_dist = dist
+            no_mais_prox = n
+    return no_mais_prox
+
+def gerando_novo_no(no_mais_prox, ponto_aleatorio, f_crescimento):
+    vet_direcao = (ponto_aleatorio[0] - no_mais_prox[0], ponto_aleatorio[1] - no_mais_prox[1])
+    modulo_vet = math.sqrt(vet_direcao[0]**2 + vet_direcao[1]**2)
+
+    if modulo_vet < f_crescimento:
+        return ponto_aleatorio
+    
+    fator = f_crescimento / modulo_vet
+    novo_no = (int(no_mais_prox[0] + vet_direcao[0] * fator), int(no_mais_prox[1] + vet_direcao[1] * fator))
+    
+    return novo_no
+
+for i in range(max_interacoes):
+    ponto_aleatorio = (random.randint(0, 350), random.randint(0, 350))  
+
+    no_mais_proximo = achando_no_proximo(arvore, ponto_aleatorio)
+    novo_no = gerando_novo_no(no_mais_proximo, ponto_aleatorio, f_crescimento)
+
+    if image_copia[novo_no[0]][novo_no[1]] == 1.0: 
+        arvore.append(novo_no)  
+        caminho.append((no_mais_proximo, novo_no)) 
+        pais.append(no_mais_proximo)
+        filhos.append(novo_no)
+
+        if math.dist(novo_no, goal) < f_crescimento:
+            print("CHEGOOOOOU")
+            break
+
+
+#Mapa em escala de cinza
+image_copia = cv2.cvtColor(image.copy(), cv2.COLOR_GRAY2RGB)
+
+#Função pra colorir as linhas entre um no e outro da arvore
+for (ponto1, ponto2) in caminho:
+    cv2.line(image_copia, ponto1[::-1], ponto2[::-1], (0, 0, 255), 1) #esse ponto[::-1] serve pra trocar x e y do lugar pq no openCV é ao contrario
+
+fig = plt.figure()
+fig.canvas.manager.set_window_title('Figura 2')
+
+plt.imshow(image_copia)
+plt.title('Caminho gerado pelo RRT')
+plt.show()
+
+#colorindo o camnho :)
+caminho_correto = []
+filho = no_mais_proximo
+pai = novo_no
+
+while(1):
+    ind = filhos.index(filho)
+    pai = pais[ind]
+    caminho_correto.append((pai, filho))
+    filho = pai
+    if(filho == robo):
+        break
+
+
+#Função pra colorir o caminho
+for (ponto1, ponto2) in caminho_correto:
+    cv2.line(image_copia, ponto1[::-1], ponto2[::-1], (255, 0, 0), 1) #esse ponto[::-1] serve pra trocar x e y do lugar pq no openCV é ao contrario
+
+fig = plt.figure()
+fig.canvas.manager.set_window_title('Figura 3')
+
+plt.imshow(image_copia)
+plt.title('Caminho do robô ate o objetivo')
+plt.show()
